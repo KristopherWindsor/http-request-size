015a8909a8d14ab2fba9270ee10ebf29a219d0ed
Kristopher Windsor <kris@get.it>
2015-07-23 04:38:37 -0700

    application now shows correct formatted output

--- src/Application.php
+++ src/Application.php
@@ -21,6 +21,26 @@ class Application {
       die("Unexpected error: " . $e->getMessage() . "\n");
     }
 
-    var_dump($stats);
+    $total_req = 0;
+    $total_size = 0;
+    foreach ($stats as $stat){
+      $total_req += $stat['requests'];
+      $total_size += $stat['size'];
+    }
+
+    printf( "Processed URL: %s\n\n", $url );
+
+    printf( "Total number of HTTP requests: %d\n", $total_req );
+    printf( "Total download size for all requests: %s bytes\n\n", number_format($total_size) );
+
+    foreach (ContentCategorizer::getAllTypes() as $type => $display_name){
+      if (isset($stats[$type]))
+        printf("%s %d request(s), %s bytes\n",
+          substr($display_name . str_repeat('.', 20), 0, 20),
+          $stats[$type]['requests'],
+          number_format($stats[$type]['size']) );
+      else
+        printf( "%s 0 requests\n", substr($display_name . str_repeat('.', 20), 0, 20) );
+    }
   }
 }
--- src/ContentCategorizer.php
+++ src/ContentCategorizer.php
@@ -3,10 +3,11 @@
 namespace HttpRequestSize;
 
 class ContentCategorizer {
-  const HTML  = 'HTML';
-  const CSS   = 'CSS';
-  const JS    = 'JS';
   const ASSET = 'ASSET';
+  const CSS   = 'CSS';
+  const HTML  = 'HTML';
+  const IMAGE = 'IMAGE';
+  const JS    = 'JS';
 
   /* Categorize a web resource based on its Content-Type.
    * This logic is heuristical as we do not list out all of the Content-Types for each category.
@@ -15,14 +16,25 @@ class ContentCategorizer {
   public static function getCategory($content_type){
     $content_type = strtolower($content_type);
 
+    if ($content_type == 'text/css')
+      return self::CSS;
     if (strpos($content_type, 'javascript') !== false)
       return self::JS;
     if (strpos($content_type, 'html') !== false)
       return self::HTML;
-    if ($content_type == 'text/css')
-      return self::CSS;
+    if (strpos($content_type, 'image') !== false)
+      return self::IMAGE;
 
     return self::ASSET;
   }
 
+  public static function getAllTypes(){
+    return [
+      self::HTML  => 'Web pages (HTML)',
+      self::IMAGE => 'Images',
+      self::CSS   => 'CSS',
+      self::JS    => 'Javascript',
+      self::ASSET => 'Other assets',
+    ];
+  }
 }
